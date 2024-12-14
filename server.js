9e456d695d23f8b783b38a5bf46453c0b1b7ac5c
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2024-12-14 00:04:55 -0500

    error logging for SIGTERM and graceful shutdown

--- server.js
+++ server.js
@@ -73,5 +73,14 @@ process.on('SIGTERM', () => {
     console.log('SIGTERM signal received: closing HTTP server');
     server.close(() => {
         console.log('HTTP server closed');
+        process.exit(0);
+    });
+});
+
+process.on('SIGINT', () => {
+    console.log('SIGINT signal received: closing HTTP server');
+    server.close(() => {
+        console.log('HTTP server closed');
+        process.exit(0);
     });
 });
